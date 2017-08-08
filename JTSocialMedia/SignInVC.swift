caa6751f30522a7e0a6c31f60fee1d381cecd9d7
BalinFromMoria <BalinFromMoria@gmail.com>
2017-08-08 14:59:40 +0200

    Pods installed

---
+++ JTSocialMedia/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  JTSocialMedia
+//
+//  Created by Jiří Tomis on 08.08.17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
