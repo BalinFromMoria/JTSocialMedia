f9227aea74e17eb07f849808a8ad6a64cdb575c0
BalinFromMoria <BalinFromMoria@gmail.com>
2017-08-09 10:54:26 +0200

    Sign in and Custom Classes completed

---
+++ JTSocialMedia/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  JTSocialMedia
+//
+//  Created by Jiří Tomis on 09.08.17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GREY: CGFloat = 120.0 / 255.0
---
+++ JTSocialMedia/FancyView.swift
@@ -0,0 +1,22 @@
+//
+//  FancyView.swift
+//  JTSocialMedia
+//
+//  Created by Jiří Tomis on 09.08.17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 4.0)
+        
+    }
+}
