417dca466cae3209ba319bd5ade92d8ff94ff980
BalinFromMoria <BalinFromMoria@gmail.com>
2017-08-15 10:07:43 +0200

    Data Service

--- JTSocialMedia/SignInVC.swift
+++ JTSocialMedia/SignInVC.swift
@@ -56,7 +56,8 @@ class SignInVC: UIViewController {
             } else {
                 print("DAMN: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         }
@@ -69,7 +70,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("DAMN: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     Auth.auth().createUser(withEmail: email, password: pwd) { (user, error) in
@@ -78,7 +80,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("DAMN: Sucessfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     }
@@ -87,7 +90,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keyResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("DAMN: Data saved to keychain \(keyResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
